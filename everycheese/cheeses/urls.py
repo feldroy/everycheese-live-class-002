b44839bbb629a1fdf61a23eff897915124b6ec54
Daniel Feldroy <daniel@feldroy.com>
2020-08-23 15:36:04 -0700

    Add cheese update view, modify template

--- everycheese/cheeses/urls.py
+++ everycheese/cheeses/urls.py
@@ -5,5 +5,6 @@ app_name = "cheeses"
 urlpatterns = [
     path(route="", view=views.CheeseListView.as_view(), name="list"),
     path(route="create/new/", view=views.CheeseCreateView.as_view(), name="add"),
+    path(route="<slug:slug>/update/", view=views.CheeseUpdateView.as_view(), name="update"),    
     path(route="<slug:slug>/", view=views.CheeseDetailView.as_view(), name="detail"),
 ]
